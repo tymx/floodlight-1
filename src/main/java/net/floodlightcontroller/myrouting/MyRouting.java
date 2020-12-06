1c6caf98a34f57b9840ac2602525fa028e4a046d
Taylor <taylorannmartinez16@gmail.com>
2020-12-05 23:44:37 -0500

    submission

--- src/main/java/net/floodlightcontroller/myrouting/MyRouting.java
+++ src/main/java/net/floodlightcontroller/myrouting/MyRouting.java
@@ -1,9 +1,7 @@
 /*******************
 
 Team members and IDs:
-Name1 ID1
-Name2 ID2
-Name3 ID3
+Taylor Martinez 5869579
 
 Github link:
 https://github.com/xxx/yyy
@@ -13,12 +11,16 @@ https://github.com/xxx/yyy
 package net.floodlightcontroller.myrouting;
 
 import java.util.Collection;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 import org.openflow.protocol.OFFlowMod;
 import org.openflow.protocol.OFMatch;
@@ -42,6 +44,7 @@ import net.floodlightcontroller.devicemanager.SwitchPort;
 
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.TreeMap;
 
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
 import net.floodlightcontroller.linkdiscovery.LinkInfo;
@@ -52,11 +55,14 @@ import net.floodlightcontroller.routing.Route;
 import net.floodlightcontroller.routing.RouteId;
 import net.floodlightcontroller.staticflowentry.IStaticFlowEntryPusherService;
 import net.floodlightcontroller.topology.NodePortTuple;
+import net.floodlightcontroller.topology.OrderedNodePair;
 
 import org.openflow.util.HexString;
 import org.openflow.util.U8;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 
 public class MyRouting implements IOFMessageListener, IFloodlightModule {
 
@@ -74,6 +80,9 @@ public class MyRouting implements IOFMessageListener, IFloodlightModule {
 	protected ILinkDiscoveryService lds;
 	protected IStaticFlowEntryPusherService flowPusher;
 	protected boolean printedTopo = false;
+	
+	protected String old_ns;
+	protected String old_nd;
 
 	@Override
 	public String getName() {
@@ -128,52 +137,269 @@ public class MyRouting implements IOFMessageListener, IFloodlightModule {
 	public void startUp(FloodlightModuleContext context) {
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 	}
-
+	
 	@Override
 	public net.floodlightcontroller.core.IListener.Command receive(
 			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-
-
+		
+		Map<Link, OrderedNodePair> edges = new TreeMap<>();
+		Map<NodePortTuple, Link> switchPortLinks = new HashMap<>();
+		Map<String, NodePortTuple> nodes = new HashMap<>();
+		Map<Long, Integer> cost = new HashMap<>();
+		Map<Long, Long> previousNode = new HashMap<>();
+		ArrayList<NodePortTuple> path = new ArrayList<NodePortTuple>();
+		List<Long> pathL = new ArrayList<>();
+		
+		switches = floodlightProvider.getAllSwitchMap();
+		links = linkProvider.getLinks();
+		
+		String netsrc, netdst;
+		
 		// Print the topology if not yet.
 		if (!printedTopo) {
 			System.out.println("*** Print topology");
-
-			// For each switch, print its neighbor switches.
+			// For each switch, print its neighbor switches.		
+			for( IOFSwitch i : switches.values()) {
+				System.out.print("switch " + i.getId() + " neighbors: ");
+				for (Link j : links.keySet()) {
+					if(j.getSrc() == i.getId()) {
+						System.out.print(j.getDst() + " ");
+					}
+				}
+				System.out.print("\n");
+			}
+				
+			old_ns = "";
+			old_nd = "";
 
 			printedTopo = true;
 		}
-
 
 		// eth is the packet sent by a switch and received by floodlight.
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx,
 				IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 
-		// We process only IP packets of type 0x0800.
+		// We process only IP packets of type 0x0800. 		
 		if (eth.getEtherType() != 0x0800) {
 			return Command.CONTINUE;
 		}
 		else{
-			System.out.println("*** New flow packet");
-
 			// Parse the incoming packet.
 			OFPacketIn pi = (OFPacketIn)msg;
 			OFMatch match = new OFMatch();
-		    match.loadFromPacket(pi.getPacketData(), pi.getInPort());	
+		    match.loadFromPacket(pi.getPacketData(), pi.getInPort());
 			
 			// Obtain source and destination IPs.
 			// ...
-			System.out.println("srcIP: " + "a.b.c.d");
-	        System.out.println("dstIP: " + "a.b.c.d");
-
+		    
+		    netsrc = match.getNetworkSourceCIDR().substring(0, (match.getNetworkSourceCIDR().length() - 3));
+		    netdst = match.getNetworkDestinationCIDR().substring(0, (match.getNetworkDestinationCIDR().length() - 3));
+		    
+		    if((netsrc.compareTo(old_ns) == 0) && (netdst.compareTo(old_nd) == 0)) {
+		    	return Command.STOP;
+		    }
+		    
+		    System.out.println("*** New flow packet");
+		    
+			System.out.println("srcIP: " + netsrc);
+	        System.out.println("dstIP: " + netdst);
+	        
+	        old_ns = netsrc;
+	        old_nd = netdst;
 
 			// Calculate the path using Dijkstra's algorithm.
-			Route route = null;
-			// ...
-			System.out.println("route: " + "1 2 3 ...");			
+	        
+	        for( IOFSwitch i : switches.values()) {
+				for (Link j : links.keySet()) {
+					if(j.getSrc() == i.getId()) {
+						NodePortTuple tuple = new NodePortTuple(i.getId(), j.getSrcPort());
+						nodes.put("S" + Long.toString(i.getId()), tuple);						
+						switchPortLinks.put(tuple, j);
+					}
+				}
+	        }
+	        
+	        Long sourceNode = Long.decode(netsrc.substring(netdst.length() - 1));
+	        
+	        Long destNode = Long.decode(netdst.substring(netdst.length() - 1));
+			
+//			1 Initialization:
+//			2 N’ = {u}
+			path.add(nodes.get(Long.toString(sourceNode)));
+			pathL.add(sourceNode);
+//			3 for all nodes v
+			for(Entry<NodePortTuple, Link> i : switchPortLinks.entrySet()) {
+				Link value = i.getValue();
+//				4 if v is a neighbor of u
+				if(value.getSrc() == sourceNode) {
+//					5 then D(v) = c(u, v)
+					if(!(value.getSrc() % 2 == 0) && !(value.getDst() % 2 == 0)) {
+						cost.put(value.getDst(), 1);
+					} else if ((value.getSrc() % 2 == 0) && (value.getDst() % 2 == 0)) {
+						cost.put(value.getDst(), 100);
+					} else {
+						cost.put(value.getDst(), 10);
+					}
+					previousNode.put(value.getDst(), value.getSrc());
+				} else {
+	//				6 else D(v) = infinity
+					if(!(cost.containsKey(value.getDst()))) {
+						cost.put(value.getDst(), 1000000);
+					}
+				}
+			}
+			
+			cost.put(sourceNode, 0);
+			
+			long minKey = 0;
+			long min = 0;
+			
+			int count = 1;
+			
+//			8 Loop
+//			15 until N’= N
+			while(pathL.size() != switches.size()) {
+//				9 find w not in N’ such that D(w) is a minimum
+				for(Entry<NodePortTuple, Link> i : switchPortLinks.entrySet()) {
+					if(previousNode.containsKey(i.getKey().getNodeId())) {
+						if(!(pathL.contains(i.getKey().getNodeId()))) {
+							if(count == 1) {
+								minKey = i.getKey().getNodeId();
+								min = cost.get(i.getKey().getNodeId());
+								count++;
+							}
+							if(cost.get(i.getKey().getNodeId()) < min) {
+								minKey = i.getKey().getNodeId();
+								min = cost.get(i.getKey().getNodeId());
+							}
+						}
+					}
+				}
+				
+//				System.out.println("Path: " + pathL.toString());
+//				System.out.println("Minkey: " + minKey);
+//				System.out.println("MinCost: " + min);
+				
+//				10 add w to N’
+				path.add(nodes.get(Long.toString(minKey)));
+				pathL.add(minKey);
+//				11 update D(v) for each neighbor v of w and not in N’:
+				for(Entry<NodePortTuple, Link> i : switchPortLinks.entrySet()) {
+					Link value = i.getValue();
+//					12 D(v) = min(D(v), D(w)+ c(w, v) ) /* new cost to v is either old cost to v or known 
+					// least path cost to w plus cost from w to v */
+					if(value.getSrc() == minKey && !(pathL.contains(value.getDst()))) {
+						if(!previousNode.containsKey(value.getDst())) {
+							previousNode.put(value.getDst(), value.getSrc());
+						}
+						
+						Long prevNode = previousNode.get(value.getDst());
+						Long prevCost = (long) 0;
+						
+						while(prevNode !=  null) {
+							prevCost += cost.get(prevNode);
+							prevNode = previousNode.get(prevNode);
+						}
+						
+						if(cost.containsKey(value.getDst()) && (cost.get(value.getDst()) == 1000000)) {
+							if(!(value.getSrc() % 2 == 0) && !(value.getDst() % 2 == 0)) {
+								cost.put(value.getDst(), prevCost.intValue() + 1);
+							} else if ((value.getSrc() % 2 == 0) && (value.getDst() % 2 == 0)) {
+								cost.put(value.getDst(), prevCost.intValue() + 100);
+							} else {
+								cost.put(value.getDst(), prevCost.intValue() + 10);
+							}
+						}
+						
+						
+						if(!(value.getSrc() % 2 == 0) && !(value.getDst() % 2 == 0)) {
+							cost.put(value.getDst(), Math.min(cost.get(value.getDst()), cost.get(value.getDst()) + 1));
+						} else if ((value.getSrc() % 2 == 0) && (value.getDst() % 2 == 0)) {
+							cost.put(value.getDst(), Math.min(cost.get(value.getDst()), cost.get(value.getDst()) + 100));
+						} else {
+							cost.put(value.getDst(), Math.min(cost.get(value.getDst()), cost.get(value.getDst()) + 10));
+						}
+					}
+				}
+				
+//				System.out.println(cost.toString());
+//				System.out.println("PN: " + previousNode.toString());
+				
+				count = 1;
+				min = 0;
+				minKey = 0;
+			}
+			
+//			System.out.println("PathL: " + pathL.toString());
+			
+			String routeString = destNode.toString();
+			Long prevNode = previousNode.get(destNode);
+			Deque<Long> queue = new LinkedList<>();
+			queue.add(destNode);
+			
+			while(prevNode != null) {
+				queue.addFirst(prevNode); 
+				routeString = prevNode + " " + routeString;
+				prevNode = previousNode.get(prevNode);
+			}
+			
+			Deque<Long> reverseQ = new LinkedList<>(queue);
+			
+//			System.out.println("Queue: " + queue);	
+			
+			Long srcNode = (long) 0;
+			Long dstNode = (long) 0;
+			
+			srcNode = queue.pollFirst();
+			
+			path.removeAll(path);
+			
+			while(!queue.isEmpty()) {
+				dstNode = queue.pollFirst();
+//				System.out.println("Queue: " + queue);
+//				System.out.println("Src: " + srcNode.toString() + " Dest: " + dstNode.toString()); 
+				for(Entry<NodePortTuple, Link> i : switchPortLinks.entrySet()) {
+					Link value = i.getValue();
+					if((value.getSrc() == srcNode) && (value.getDst() == dstNode)) {
+						path.add(new NodePortTuple(value.getSrc(), value.getSrcPort()));
+						path.add(new NodePortTuple(value.getDst(), value.getDstPort()));
+					}
+				}
+				srcNode = dstNode;
+			}
+			
+			srcNode = reverseQ.pollLast();
+			
+			ArrayList<NodePortTuple> reversePath = new ArrayList<NodePortTuple>();
+			
+			while(!reverseQ.isEmpty()) {
+				dstNode = reverseQ.pollLast();
+//				System.out.println("Queue: " + queue);
+//				System.out.println("Src: " + srcNode.toString() + " Dest: " + dstNode.toString()); 
+				for(Entry<NodePortTuple, Link> i : switchPortLinks.entrySet()) {
+					Link value = i.getValue();
+					if((value.getSrc() == srcNode) && (value.getDst() == dstNode)) {
+						reversePath.add(new NodePortTuple(value.getSrc(), value.getSrcPort()));
+						reversePath.add(new NodePortTuple(value.getDst(), value.getDstPort()));
+					}
+				}
+				srcNode = dstNode;
+			}
+	        
+//			Route route = null;
+			Route route = new Route(new RouteId(sourceNode, destNode), path);
+			Route revRoute = new Route(new RouteId(destNode, sourceNode), reversePath);
+			
+			if(path != null) {
+				System.out.println("Path: " + path.toString());
+			}
+			
+			System.out.println("route: " + routeString);
 
 			// Write the path into the flow tables of the switches on the path.
 			if (route != null) {
 				installRoute(route.getPath(), match);
+				installRoute(revRoute.getPath(), new OFMatch());
 			}
 			
 			return Command.STOP;
